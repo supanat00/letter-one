cfbe67f8d3a67e536aa27f61adbd49203321b5b3
supanat00 <supanat90@gmail.com>
2023-08-20 18:23:14 +0700

    update-music-auto

--- src/pages/SplashScreen.jsx
+++ src/pages/SplashScreen.jsx
@@ -1,27 +1,24 @@
-import { useEffect } from 'react';
+import React from 'react';
 import { useNavigate } from 'react-router-dom';
 
 function SplashScreen() {
   const navigate = useNavigate();
 
-  useEffect(() => {
-    const timeout = setTimeout(() => {
-      navigate('/HomePage'); // Redirect to the home page after 3 seconds
-    }, 3000);
-    return () => clearTimeout(timeout);
-  }, [navigate]);
+  const handlePageChange = () => {
+    navigate('/HomePage'); // Redirect to the home page
+  };
 
   return (
-    <div className="App-splash waveTextAnimated welcome-text">
-      <span>L</span>
-      <span>O</span>
-      <span>V</span>
-      <span>E</span>
-      <span>S.</span>
+    <div className="App-splash" onClick={handlePageChange} onTouchEnd={handlePageChange}>
+      <div className="waveTextAnimated welcome-text">
+        <span>L</span>
+        <span>O</span>
+        <span>V</span>
+        <span>E</span>
+        <span>S.</span>
+      </div>
     </div>
   );
 }
-
-
 
 export default SplashScreen;
