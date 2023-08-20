656b6846e10639ca5eb9cac963ba845f46892e37
supanat00 <supanat90@gmail.com>
2023-08-20 18:35:44 +0700

    update18

--- src/components/PartOne.jsx
+++ src/components/PartOne.jsx
@@ -1,27 +1,8 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import symbol1 from '../icon/symbol_1.png';
 
-function PartOne ()  {
-    const [scrollY, setScrollY] = useState(0);
-
-    const handleScroll = () => {
-        setScrollY(window.scrollY);
-      };
-    
-      const handleOrientation = (event) => {
-        const angle = event.beta;
-        const parallaxAmount = angle * 0.5; // Adjust this value as needed
-        setScrollY(-parallaxAmount);
-      };
-
-      useEffect(() => {
-        window.addEventListener('scroll', handleScroll);
-        window.addEventListener('deviceorientation', handleOrientation);
-        return () => {
-          window.removeEventListener('scroll', handleScroll);
-          window.removeEventListener('deviceorientation', handleOrientation);
-        };
-      }, []);
+function PartOne ()  {    
+     
   return (
     <div className='partone'>
 
@@ -33,7 +14,7 @@ function PartOne ()  {
         THE WEDDING OF
       </div>
 
-      <div style={{ transform: `rotateY(${scrollY}deg)` }}>        
+      <div >        
         <img src={symbol1} className='symbol-one' loading='lazy' alt="logo" />
       </div>
     </div>
